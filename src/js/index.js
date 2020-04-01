5ee04b29540bfcaac21424a6a067f9d0a653e64f
Roman <r.sulima@finik.pro>
2020-04-01 21:53:24 +0300

    FINAL PROJECT COMMIT

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
 $('.info').waypoint( () => {
        $('nav').toggleClass('stick');
 });
+
+// animations
+
+$('.howItWorks').waypoint( () => {
+     $('.firstOfTwo').addClass('visible').addClass('animated').addClass('bounceInUp');
+}, {
+       offset : '50%'
+});
+
+$('.tariffBlock').waypoint( () => {
+       $('.tariffBlock').addClass('visible').addClass('animated').addClass('zoomIn');
+}, {
+       offset : '80%'
+});
+
+// sticky menu
+
+$('.fa-times').click( () => {
+       $('.headerList').slideToggle(500);
+       $('.fa-times').css('display','none');
+       $('.fa-bars').css('display', 'block');
+});
+
+$('.fa-bars').click( () => {
+       $('.fa-bars').css('display', 'none');
+       $('.headerList').slideToggle(500);
+       $('.fa-times').css('display','block');
+});
